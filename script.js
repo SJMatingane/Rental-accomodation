c231f2a0babeef842f04833840df952c171e97af
Sikhulile Jim Matingane <139535954+SJMatingane@users.noreply.github.com>
2024-08-26 17:00:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const aboutSection = document.querySelector("section h2");
+
+    aboutSection.addEventListener("click", function() {
+        alert("Hello! Thanks for checking out Our Accamondation at LATOVES ESTATE...");
+    });
+});
